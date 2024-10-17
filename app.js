03669f13d96ad55f149cc299aad026987669304a
Ana Muller <anamullerpsi@gmail.com>
2024-10-17 15:11:41 -0300

    Replica ML, no responsive

--- app.js
+++ app.js
@@ -42,3 +42,33 @@ document.getElementById("arrow-left").addEventListener("click", slide); //click 
 document.getElementById("arrow-right").addEventListener("click", slide); //click derecho para cambiar imagen
 
 //SLIDE SECTION CARDS
+
+// const cardsContainer = document.querySelector('.cards');
+// const cardElements = document.querySelectorAll('.product-card');
+// const arrowLeft = document.querySelector('.ar-left');
+// const arrowRight = document.querySelector('.ar-right');
+
+
+
+
+
+
+// // Selecciona los elementos
+// const cardsContainer = document.querySelector('.cards');
+// const arrowLeft = document.querySelector('.ar-left');
+// const arrowRight = document.querySelector('.ar-right');
+
+// // Desplazamiento en píxeles para cada clic (puede ser el ancho de una tarjeta o un poco más)
+// const scrollAmount = 150 * 6; // Ancho de 6 tarjetas
+
+// // Mover hacia la derecha
+// arrowRight.addEventListener('click', () => {
+//   cardsContainer.scrollLeft += scrollAmount; // Desplaza a la derecha
+// });
+
+// // Mover hacia la izquierda
+// arrowLeft.addEventListener('click', () => {
+//   cardsContainer.scrollLeft -= scrollAmount; // Desplaza a la izquierda
+// });
+
+  
